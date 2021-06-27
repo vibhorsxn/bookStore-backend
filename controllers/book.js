ed55bc41b04ecf984a123e0273fad6e100062ca4
Vibhor <vibhorsxn@gmail.com>
2021-06-27 18:13:49 +0530

    Add static book json data

---
+++ controllers/book.js
@@ -0,0 +1,25 @@
+exports.allbook=(req,res)=>{
+    res.json([
+
+    {   
+        "id":1,
+        "title": "13 reasons why?",
+        "author":"Jay Asher",
+    },
+    {   "id":2,
+        "title": "A fine balance",
+        "author":"Rohinton Mistry",
+    },{ "id":3,
+        "title": "God of small palace",
+        "author":"Arundhati Roy",
+    },{ "id":4,
+        "title": "The inheritance of loss",
+        "author":"Kiran Desai",
+    },{ "id":5,
+        "title": "Maximum city",
+        "author":"Suketu Mehta",
+    },
+    
+    
+]);
+};
